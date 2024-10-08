ba4dddcf4484ff796df05752e11c47ca4788e847
Nyadej <naomiadejumo@gmail.com>
2024-10-08 11:48:29 +0100

    added multiplication scenarios for positive, negative, and zero cases

--- src/test/java/com/codingblackfemales/CalculatorTests.java
+++ src/test/java/com/codingblackfemales/CalculatorTests.java
@@ -23,4 +23,38 @@ public class CalculatorTests {
 
         assertEquals(-2, difference);
     }
+
+    @Test
+    @DisplayName("multiply two numbers")
+    public void testMultiplicationTwoPositiveNumbers() {
+        final Integer multiplication = Calculator.multiply(2, 4);
+
+        assertEquals(8, multiplication);
+    }
+
+    @Test
+    @DisplayName("multiply a positive and a negative number")
+    public void testMultiplicationTwoPositiveAndNegativeNumber() {
+        final Integer multiplication = Calculator.multiply(2, -4);
+
+        assertEquals(-8, multiplication);
+    }
+
+    @Test
+    @DisplayName("multiply two number numbers")
+    public void testMultiplicationTwoNegativeNumbers() {
+        final Integer multiplication = Calculator.multiply(-2, -4);
+
+        assertEquals(8, multiplication);
+    }
+
+    @Test
+    @DisplayName("multiplying by zero")
+    public void testMultiplicationByZero() {
+        final Integer multiplication = Calculator.multiply(2, 0);
+
+        assertEquals(0, multiplication);
+    }
+
 }
+
